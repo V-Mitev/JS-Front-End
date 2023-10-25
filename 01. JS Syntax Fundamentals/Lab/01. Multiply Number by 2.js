32859e98cbafc63cea7f486a3aa94327547c66b4
Vladimir Mitev <vmitev2001@abv.bg>
2023-10-25 22:26:37 +0300

    Add first two tasks

---
+++ 01. JS Syntax Fundamentals/Lab/01. Multiply Number by 2.js
@@ -0,0 +1,7 @@
+function multiplyNumberByTwo(num) {
+	let result = num * 2;
+
+	return result;
+}
+
+console.log(multiplyNumberByTwo(2));
---
+++ 01. JS Syntax Fundamentals/Lab/02. Student Information.js
@@ -0,0 +1,5 @@
+function PrintStudentInformation (name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+PrintStudentInformation('Petar', 15, 5.65);
