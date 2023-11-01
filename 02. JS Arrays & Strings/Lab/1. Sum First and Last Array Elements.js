b85b7e506d8d4403c41d8e75959de378ce05602f
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-01 22:16:36 +0200

    Add task one

---
+++ 02. JS Arrays & Strings/Lab/1. Sum First and Last Array Elements.js
@@ -0,0 +1,8 @@
+function solve(input){
+    let firstArrayElement = input[0];
+    let lastArrayElement = input[input.length - 1]
+
+    console.log(firstArrayElement + lastArrayElement)
+}
+
+solve([20, 30, 40]);
