1d8444c3e1aa72098f7ad7a98e7aa9967d829e5f
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-03 19:36:19 +0200

    Add task six

---
+++ 02. JS Arrays & Strings/Exercises/5. Reveal Words.js
@@ -0,0 +1,23 @@
+function solve(words, text) {
+	let wordsArray = words.split(", ");
+	let textArray = text.split(" ");
+
+	for (let i = 0; i < wordsArray.length; i++) {
+		for (let j = 0; j < textArray.length; j++) {
+			let isMatch =
+				textArray[j].includes("*") &&
+				textArray[j].length === wordsArray[i].length;
+
+			if (isMatch) {
+                textArray[j] = wordsArray[i];
+			}
+		}
+	}
+
+	console.log(textArray.join(" "));
+}
+
+solve(
+	"great, learning",
+	"softuni is ***** place for ******** new programming languages"
+);
