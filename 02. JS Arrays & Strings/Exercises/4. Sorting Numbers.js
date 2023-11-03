f689ee42a1ebc46d00864da871a2ca9c1bd78267
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-03 09:13:51 +0200

    Edit logic in task four

--- 02. JS Arrays & Strings/Exercises/4. Sorting Numbers.js
+++ 02. JS Arrays & Strings/Exercises/4. Sorting Numbers.js
@@ -1,37 +1,33 @@
 function solve(array) {
-	let newArray = [];
 	let orderedArray = [];
 
-	array.forEach((element) => {
-		newArray.push(element);
-	});
-
-	while (newArray.length > 0) {
+	while (array.length > 0) {
 		let smallestNumber = Number.MAX_VALUE;
 		let indexOfSmallestNum = 0;
 		let biggestNumber = Number.MIN_VALUE;
 		let indexOfBigestNum = 0;
 
-		for (let i = 0; i <= newArray.length - 1; i++) {
-			if (smallestNumber > newArray[i]) {
-				smallestNumber = newArray[i];
-				indexOfSmallestNum = newArray.indexOf(smallestNumber);
+		for (let i = 0; i <= array.length - 1; i++) {
+			if (smallestNumber > array[i]) {
+				smallestNumber = array[i];
 			}
 
-			if (biggestNumber < newArray[i]) {
-				biggestNumber = newArray[i];
-				indexOfBigestNum = newArray.indexOf(biggestNumber);
+			if (biggestNumber < array[i]) {
+				biggestNumber = array[i];
 			}
 		}
 
-		newArray.splice(indexOfSmallestNum, 1);
-		newArray.splice(indexOfBigestNum, 1);
+		indexOfSmallestNum = array.indexOf(smallestNumber);
+		array.splice(indexOfSmallestNum, 1);
+
+		indexOfBigestNum = array.indexOf(biggestNumber);
+		array.splice(indexOfBigestNum, 1);
 
 		orderedArray.push(smallestNumber);
 		orderedArray.push(biggestNumber);
 	}
 
-	console.log(orderedArray);
+	console.log(orderedArray.join('\n'));
 }
 
 solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
