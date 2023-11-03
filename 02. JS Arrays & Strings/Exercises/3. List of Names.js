ed3529dedd3e05b9f532bca8ec78edf53ec4bc53
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-03 18:55:36 +0200

    Edit task three

--- 02. JS Arrays & Strings/Exercises/3. List of Names.js
+++ 02. JS Arrays & Strings/Exercises/3. List of Names.js
@@ -1,16 +1,15 @@
-function solve(array) {
-	let newArray = [];
-	for (let i = 0; i <= array.length - 1; i++) {
-		newArray.push(array[i]);
+function solve(list) {
+	
+	list.sort((a, b) => {
+		return a.localeCompare(b);
+	});
+
+	let index = 1;
+	
+	for (const name of list) {
+		console.log(`${index}.${name}`);
+		index++;
 	}
-
-	newArray.sort();
-
-	for (let i = 0; i <= array.length - 1; i++) {
-		newArray[i] = `${i + 1}.${newArray[i]}`;
-	}
-
-	console.log(newArray.join("\n"));
 }
 
 solve(["John", "Bob", "Christina", "Ema"]);
