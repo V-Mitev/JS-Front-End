b2bcbc74b46963525a84ae60d3130936c2e46faa
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-24 23:09:16 +0200

    Solve task two and edit first task

--- 05. DOM Introduction/Lab/01.Edit Element/EditElement.js
+++ 05. DOM Introduction/Lab/01.Edit Element/EditElement.js
@@ -1,5 +1,9 @@
 function editElement(reference, match, replacer) {
-    let result = reference.textContent.replace(match, replacer);
+    let content = reference.textContent;
+
+    let matcher = new RegExp(match, 'g');
+
+    let result = content.replace(matcher, replacer);
 
     return reference.textContent = result;
 }
--- 05. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ 05. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+	let liItems = document.querySelectorAll("#items li");
+
+	let textArea = document.querySelector("#result");
+
+	for (const li of liItems) {
+		textArea.value += li.textContent + "\n";
+	}
 }
