1f6707a4f1bacf20134ae9d3e22c2dd69d1f6f56
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-26 20:30:10 +0200

    Add task one from dom introduction exercises

--- 05. DOM Introduction/Exercise/01. Subtraction/subtract.js
+++ 05. DOM Introduction/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    const numberOne = Number(document.getElementById('firstNumber').value);
+    const numberTwo = Number(document.getElementById('secondNumber').value);
+
+    let result = document.getElementById('result');
+
+    result.innerHTML = numberOne - numberTwo;
 }
