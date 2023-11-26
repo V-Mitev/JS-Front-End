f97de2e93ef33e5a02d2c18cf0169ca5c06b11ca
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-26 21:25:49 +0200

    Add task three

--- 05. DOM Introduction/Exercise/03. Accordion/accordion.js
+++ 05. DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.getElementsByClassName('button')[0];
+    let text = document.getElementById('extra');
+
+    if (button.innerHTML === 'More') {
+        button.innerHTML = 'Less';
+        text.style.display = 'block';
+    } else {
+        button.innerHTML = 'More';
+        text.style.display = 'none';
+    }
 }
