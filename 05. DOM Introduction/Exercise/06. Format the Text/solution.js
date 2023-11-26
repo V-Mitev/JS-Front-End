3017d8b49787ef44d2565e529bf8b123abdb1e5a
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-26 23:51:38 +0200

    Add task six

--- 05. DOM Introduction/Exercise/06. Format the Text/solution.js
+++ 05. DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+	let input = document.getElementById("input").value.split(".");
+	let output = document.getElementById("output");
+
+	input = input
+  .filter((s) => s.length > 0)
+  .map((s) => (s += "."));
+
+	while (input.length > 0) {
+		let p = document.createElement("p");
+
+		p.innerHTML = input.splice(0, 3).join("");
+
+		output.appendChild(p);
+	}
 }
