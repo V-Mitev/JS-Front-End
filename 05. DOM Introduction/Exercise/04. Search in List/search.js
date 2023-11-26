bad9640e8ad494e862df855ff82c8c47ebdff3a5
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-26 21:54:21 +0200

    Add task four

--- 05. DOM Introduction/Exercise/04. Search in List/search.js
+++ 05. DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+	let listItems = document.getElementsByTagName("li");
+	let searchText = document.getElementById("searchText").value;
+	let result = document.getElementById("result");
+	let matches = 0;
+
+	for (const li of listItems) {
+		li.style.fontWeight = "normal";
+		li.style.textDecoration = "none";
+	}
+
+	for (const li of listItems) {
+		if (li.innerText.includes(searchText)) {
+			matches++;
+
+			li.style.fontWeight = "bold";
+			li.style.textDecoration = "underline";
+		}
+	}
+
+	result.innerHTML = `${matches} matches found`;
 }
