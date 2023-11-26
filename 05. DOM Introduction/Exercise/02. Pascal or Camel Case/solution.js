1c1ffe11b2ebb7c96232aca50ff2aecb30cbf15e
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-26 21:19:26 +0200

    Add task two

--- 05. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ 05. DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value.toLowerCase().split(' ');
+  let convention = document.getElementById('naming-convention').value;
+
+  let result = document.getElementById('result');
+
+  let resultArray = [];
+  
+  if (convention === 'Camel Case') {
+    resultArray.push(text[0]);
+
+    for (let i = 1; i < text.length; i++) {
+      text[i] = text[i].charAt(0).toUpperCase() + text[i].slice(1);
+
+      resultArray.push(text[i]);
+    }
+  } else if (convention === 'Pascal Case') {
+    for (let i = 0; i < text.length; i++) {
+      text[i] = text[i].charAt(0).toUpperCase() + text[i].slice(1);
+
+      resultArray.push(text[i]);
+    }
+  } else {
+    resultArray.push('Error!');
+  }
+
+  result.innerHTML = resultArray.join('');
 }
