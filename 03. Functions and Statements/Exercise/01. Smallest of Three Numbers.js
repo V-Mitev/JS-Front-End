417b2110cb9e98c6f43070b884bd74e82e43a3a7
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-27 17:35:51 +0200

    Add exercises document and task one

---
+++ 03. Functions and Statements/Exercise/01. Smallest of Three Numbers.js
@@ -0,0 +1,11 @@
+function solve(num1, num2, num3) {
+    if (num1 < num2 && num1 < num3) {
+        console.log(num1);
+    } else if (num2 < num1 && num2 < num3) {
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+
+solve(98, 555, 37);
