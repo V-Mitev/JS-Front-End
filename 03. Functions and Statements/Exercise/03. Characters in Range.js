547c21c7d7d7c4c74d0d41129f0fe41ea732ef0e
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-27 17:56:25 +0200

    Add task three

---
+++ 03. Functions and Statements/Exercise/03. Characters in Range.js
@@ -0,0 +1,15 @@
+function solve(char1, char2) {
+	let result = [];
+
+	if (char1 > char2) {
+		[char1, char2] = [char2, char1];
+	}
+
+	for (let i = char1.charCodeAt(0) + 1; i < char2.charCodeAt(0); i++) {
+		result.push(String.fromCharCode(i));
+	}
+
+	console.log(result.join(" "));
+}
+
+solve("r", "a");
