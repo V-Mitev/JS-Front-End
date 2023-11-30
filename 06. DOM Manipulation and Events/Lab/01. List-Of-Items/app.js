9eeef30b38d74afbeb98a66202e69ebf74cabded
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-30 15:54:04 +0200

    Add task one

--- 06. DOM Manipulation and Events/Lab/01. List-Of-Items/app.js
+++ 06. DOM Manipulation and Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+	let ulList = document.getElementById("items");
+	let text = document.getElementById("newItemText");
+
+	let newItem = document.createElement("li");
+
+	newItem.textContent = text.value;
+
+	ulList.appendChild(newItem);
+
+    text.value = '';
 }
