bcab1d51393c51045b89fa1d1e8a5633f53fe59a
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-30 16:49:30 +0200

    Add task three

--- 06. DOM Manipulation and Events/Lab/03. Add-Delete/app.js
+++ 06. DOM Manipulation and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,25 @@
 function addItem() {
-    //TODO...
+    
+	let ulList = document.getElementById("items");
+	let text = document.getElementById("newItemText");
+
+	let newItem = document.createElement("li");
+
+	let a = document.createElement("a");
+
+	a.textContent = "[Delete]";
+	a.href = "#";
+
+	newItem.textContent = text.value;
+	newItem.appendChild(a);
+
+	ulList.appendChild(newItem);
+
+	text.value = "";
+
+	a.addEventListener("click", deleteElement);
+
+	function deleteElement() {
+		newItem.remove();
+	}
 }
