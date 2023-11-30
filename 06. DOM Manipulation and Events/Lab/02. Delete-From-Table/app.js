58c531e01eb6a9bcbe5bd3e23d8011c25c7304a5
Vladimir Mitev <vmitev2001@abv.bg>
2023-11-30 16:30:46 +0200

    Add task two

--- 06. DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
+++ 06. DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+	let rows = Array.from(document.querySelectorAll("tbody tr"));
+	let input = (emailInput = document.getElementsByName("email")[0].value);
+	let result = document.getElementById("result");
+
+	for (const row of rows) {
+		let email = row.children[1].textContent;
+
+		if (email === input) {
+			row.remove();
+			result.textContent = "Deleted.";
+		} else {
+			result.textContent = "Not found.";
+		}
+	}
 }
