c180763df71d350b512e1878c6ea705bdb11396d
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-04 21:35:52 +0200

    Add task six

--- 06. DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
+++ 06. DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+	let inputs = Array.from(document.querySelectorAll("input"));
+
+	for (const input of inputs) {
+		input.addEventListener("focus", focused);
+		input.addEventListener("blur", blured);
+	}
+
+	function focused(e) {
+		e.target.parentElement.classList = "focused";
+	}
+
+	function blured(e) {
+		e.target.parentElement.classList.remove("focused");
+	}
 }
