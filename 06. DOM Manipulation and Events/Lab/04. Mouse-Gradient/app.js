13cc685c0d6f75f0c0160713efb211813c05029a
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-04 21:14:10 +0200

    Add task four

--- 06. DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
+++ 06. DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,19 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+	const gradient = document.getElementById("gradient");
+	const result = document.getElementById("result");
+
+	gradient.addEventListener("mousemove", gradientMove);
+	gradient.addEventListener("mouseout", gradientOut);
+
+	function gradientMove(event) {
+		let power = event.offsetX / (event.target.clientWidth - 1);
+
+		power = Math.trunc(power * 100);
+
+		result.textContent = power + "%";
+	}
+
+	function gradientOut(event) {
+		document.getElementById("result").textContent = "";
+	}
 }
