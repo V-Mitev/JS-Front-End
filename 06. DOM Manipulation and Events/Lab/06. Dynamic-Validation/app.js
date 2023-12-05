0de0cf3198d2dbbada0910ee7990fff911fd3079
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-05 23:57:27 +0200

    Add task six

--- 06. DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
+++ 06. DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+	let email = document.getElementById("email");
+
+	email.addEventListener("change", change);
+
+	function change(e) {
+		const regex = new RegExp("^[a-z]+@[a-z]+.[a-z]+", "g");
+
+		let isMatching = regex.test(e.target.value);
+
+		if (!isMatching && e.target.value.length > 0) {
+			e.target.classList = "error";
+		} else {
+            e.target.classList.remove('error');
+        }
+	}
 }
