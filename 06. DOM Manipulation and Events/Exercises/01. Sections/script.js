2833f0b484215765335b535b39d754e04da4e388
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-06 14:25:50 +0200

    Add exercise one

--- 06. DOM Manipulation and Events/Exercises/01. Sections/script.js
+++ 06. DOM Manipulation and Events/Exercises/01. Sections/script.js
@@ -1,3 +1,27 @@
 function create(words) {
-   console.log('TODO:...');
+	const content = document.getElementById("content");
+
+	for (const word of words) {
+		const div = document.createElement("div");
+
+		const p = document.createElement("p");
+
+		p.textContent = word;
+		p.style.display = "none";
+
+		div.appendChild(p);
+		content.appendChild(div);
+
+		div.addEventListener("click", click);
+	}
+
+	function click(e) {
+		const p = e.target.querySelector("p");
+
+		if (p.style.display === "none") {
+			p.style.display = "";
+		} else {
+			p.style.display = "none";
+		}
+	}
 }
