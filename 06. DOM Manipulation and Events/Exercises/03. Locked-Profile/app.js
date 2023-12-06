ce2f70d6ad3cd2f1e86f6523fa83074afdbf6567
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-06 22:14:36 +0200

    Add task three

--- 06. DOM Manipulation and Events/Exercises/03. Locked-Profile/app.js
+++ 06. DOM Manipulation and Events/Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,31 @@
 function lockedProfile() {
-    console.log('TODO...')
+	const buttons = Array.from(document.getElementsByTagName("button"));
+
+	for (const button of buttons) {
+		button.addEventListener("click", showInformation);
+	}
+
+	function showInformation(e) {
+        let btn = e.target;
+		let profile = btn.parentElement;
+
+        let childrens = profile.children;
+
+        let lockInput = childrens[2];
+        let info = childrens[9];
+
+        if (lockInput.checked) {
+            return;
+        }
+
+        if (btn.textContent === "Show more") {
+            info.style.display = 'block';
+
+            btn.textContent = "Hide it";
+        } else {
+            info.style.display = 'none';
+            
+            btn.textContent = "Show more";
+        }
+	}
 }
