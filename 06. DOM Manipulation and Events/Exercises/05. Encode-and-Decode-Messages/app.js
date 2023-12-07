b638bd4c067389ed18f13873d24b62e5725d0f3e
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-07 12:43:38 +0200

    Add task five

--- 06. DOM Manipulation and Events/Exercises/05. Encode-and-Decode-Messages/app.js
+++ 06. DOM Manipulation and Events/Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,32 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+	let mainChildrens = document.getElementById("main").children;
+
+	let encodeButton = mainChildrens[0].children[2];
+	let decodeButton = mainChildrens[1].children[2];
+
+	encodeButton.addEventListener("click", encode);
+	decodeButton.addEventListener("click", decode);
+
+	let encodeTextArea = mainChildrens[0].children[1];
+	let decodeTextArea = mainChildrens[1].children[1];
+
+	function encode() {
+        decodeTextArea.value = '';
+
+		for (let char of encodeTextArea.value.split("")) {
+			decodeTextArea.value += String.fromCharCode(char.charCodeAt() + 1);
+		}
+
+		encodeTextArea.value = "";
+	}
+
+	function decode() {
+        let decodedMessage = '';
+
+		for (let char of decodeTextArea.value.split('')) {
+			decodedMessage += String.fromCharCode(char.charCodeAt() - 1);
+		}
+
+        decodeTextArea.value = decodedMessage;
+	}
 }
