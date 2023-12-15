87318c30c7b39034472ec1f8f77d726ada1661e0
Vladimir Mitev <vmitev2001@abv.bg>
2023-12-15 12:52:30 +0200

    Add files

--- 06. DOM Manipulation and Events/Exercises/06. Furniture/solution.js
+++ 06. DOM Manipulation and Events/Exercises/06. Furniture/solution.js
@@ -1,4 +1,54 @@
 function solve() {
+	// let exerciseContainer = document.getElementById("exercise").childNodes;
 
-  //TODO...
+	// let textAreaInput = exerciseContainer[3];
+	// let genereteButton = exerciseContainer[5];
+  // let buyButton = exerciseContainer[11];
+
+  // let table = document.getElementsByClassName("table")[0];
+  // let tbody = table.children[1];
+
+	// genereteButton.addEventListener("click", addFurniture);
+  // buyButton.addEventListener("click", buyFurniture);
+
+	// function addFurniture(e) {
+	// 	let inputText = JSON.parse(textAreaInput.value);
+
+	// 	for (let i = 0; i < inputText.length; i++) {
+	// 		const element = inputText[i];
+
+	// 		let img = document.createElement("img");
+	// 		let input = document.createElement("input");
+
+	// 		img.src = element["img"];
+	// 		input.type = "checkbox";
+
+	// 		let row = document.createElement("tr");
+
+	// 		let tdName = document.createElement("td");
+	// 		let tdPrice = document.createElement("td");
+	// 		let tdImage = document.createElement("td");
+	// 		let tdDecFactor = document.createElement("td");
+	// 		let tdCheckBox = document.createElement("td");
+
+	// 		tdName.innerText = element["name"];
+	// 		tdPrice.innerText = element["price"];
+	// 		tdDecFactor.innerText = element["decFactor"];
+
+  //     tdImage.appendChild(img);
+	// 		tdCheckBox.appendChild(input);
+
+	// 		row.appendChild(tdImage);
+	// 		row.appendChild(tdName);
+	// 		row.appendChild(tdPrice);
+	// 		row.appendChild(tdDecFactor);
+	// 		row.appendChild(tdCheckBox);
+
+	// 		tbody.appendChild(row);
+	// 	}
+	// }
+
+  // function buyFurniture(e) {
+
+  // }
 }
